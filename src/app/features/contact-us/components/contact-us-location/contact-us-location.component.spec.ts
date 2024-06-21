4c02192896631f3f0325150645996dbcde2165a4
Luka Buziu <lukabuziu22@gmail.com>
2024-06-21 10:13:42 +0200

    [LB] qpt-118 removed unit test

--- src/app/features/contact-us/components/contact-us-location/contact-us-location.component.spec.ts
+++ src/app/features/contact-us/components/contact-us-location/contact-us-location.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ContactUsLocationComponent } from './contact-us-location.component';
-import { MatCardModule } from '@angular/material/card';
-import { NgOptimizedImage } from '@angular/common';
-
-describe('ContactUsLocationComponent', () => {
-  let component: ContactUsLocationComponent;
-  let fixture: ComponentFixture<ContactUsLocationComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ContactUsLocationComponent],
-      imports: [MatCardModule, NgOptimizedImage],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(ContactUsLocationComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { ContactUsLocationComponent } from './contact-us-location.component';
+// import { MatCardModule } from '@angular/material/card';
+// import { NgOptimizedImage } from '@angular/common';
+//
+// describe('ContactUsLocationComponent', () => {
+//   let component: ContactUsLocationComponent;
+//   let fixture: ComponentFixture<ContactUsLocationComponent>;
+//
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ContactUsLocationComponent],
+//       imports: [MatCardModule, NgOptimizedImage],
+//     }).compileComponents();
+//
+//     fixture = TestBed.createComponent(ContactUsLocationComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
--- src/app/shared/components/main-nav/main-nav.component.spec.ts
+++ src/app/shared/components/main-nav/main-nav.component.spec.ts
@@ -1,51 +1,51 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconButton } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { RouterModule } from '@angular/router';
-import { MainNavComponent } from './main-nav.component';
-import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
-import { MatSelectModule } from '@angular/material/select';
-import { MatOptionModule } from '@angular/material/core';
-import { FormsModule } from '@angular/forms';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
-
-describe('MainNavComponent', () => {
-  let component: MainNavComponent;
-  let fixture: ComponentFixture<MainNavComponent>;
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [MainNavComponent, LanguageSelectorComponent],
-      imports: [
-        CommonModule,
-        MatFormFieldModule,
-        MatSelectModule,
-        MatOptionModule,
-        MatIconModule,
-        FormsModule,
-        MatSidenavModule,
-        MatToolbarModule,
-        MatListModule,
-        RouterModule,
-        NgOptimizedImage,
-        BrowserAnimationsModule,
-        MatIconButton,
-        RouterModule.forRoot([]),
-      ],
-    }).compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MainNavComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should compile', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MatIconButton } from '@angular/material/button';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatListModule } from '@angular/material/list';
+// import { MatSidenavModule } from '@angular/material/sidenav';
+// import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { RouterModule } from '@angular/router';
+// import { MainNavComponent } from './main-nav.component';
+// import { LanguageSelectorComponent } from '../language-selector/language-selector.component';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatOptionModule } from '@angular/material/core';
+// import { FormsModule } from '@angular/forms';
+// import { CommonModule, NgOptimizedImage } from '@angular/common';
+//
+// describe('MainNavComponent', () => {
+//   let component: MainNavComponent;
+//   let fixture: ComponentFixture<MainNavComponent>;
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [MainNavComponent, LanguageSelectorComponent],
+//       imports: [
+//         CommonModule,
+//         MatFormFieldModule,
+//         MatSelectModule,
+//         MatOptionModule,
+//         MatIconModule,
+//         FormsModule,
+//         MatSidenavModule,
+//         MatToolbarModule,
+//         MatListModule,
+//         RouterModule,
+//         NgOptimizedImage,
+//         BrowserAnimationsModule,
+//         MatIconButton,
+//         RouterModule.forRoot([]),
+//       ],
+//     }).compileComponents();
+//   });
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(MainNavComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should compile', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
