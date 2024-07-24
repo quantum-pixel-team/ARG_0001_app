3fe3a2989d35f982cbaa21662509390089c642c7
Luka Buziu <lukabuziu22@gmail.com>
2024-07-24 17:27:12 +0200

    [LB] qpt-161 Added hotel room animation

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { register } from 'swiper/element/bundle';
+import 'hammerjs'
 
 // register Swiper custom elements
 register();
