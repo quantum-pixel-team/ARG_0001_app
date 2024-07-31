1c6e3098fb3437f891ee4a4ac449eddb2f4be451
Indrit Vaka <indrit.vaka-extern@gmail.com>
2024-07-31 12:09:30 +0200

    [IV] QPT-174 added room images

---
+++ src/app/features/hotel/components/hotel-room-card-images/hotel-room-card-images.component.spec.ts
@@ -0,0 +1,23 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { HotelRoomCardImagesComponent } from './hotel-room-card-images.component';
+//
+// describe('HotelRoomCardImagesComponent', () => {
+//   let component: HotelRoomCardImagesComponent;
+//   let fixture: ComponentFixture<HotelRoomCardImagesComponent>;
+//
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [HotelRoomCardImagesComponent]
+//     })
+//     .compileComponents();
+//
+//     fixture = TestBed.createComponent(HotelRoomCardImagesComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
---
+++ src/app/features/hotel/components/hotel-room-card-images/hotel-room-card-images.component.ts
@@ -0,0 +1,16 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from "@angular/core";
+import { HotelRoom } from "../../interfaces/room";
+import { MatCardXlImage } from "@angular/material/card";
+import { NgForOf, NgOptimizedImage } from "@angular/common";
+
+@Component({
+  selector: 'app-hotel-room-card-images',
+  standalone: true,
+  imports: [MatCardXlImage, NgOptimizedImage, NgForOf],
+  templateUrl: './hotel-room-card-images.component.html',
+  styleUrl: './hotel-room-card-images.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class HotelRoomCardImagesComponent {
+  @Input() room!: HotelRoom;
+}
