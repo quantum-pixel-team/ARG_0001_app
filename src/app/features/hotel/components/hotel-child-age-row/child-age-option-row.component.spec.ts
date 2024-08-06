0ec0480c1f79a6c65d3339159726af77b554e0bb
Luka Buziu <lukabuziu22@gmail.com>
2024-08-06 17:48:45 +0200

    [LB] qpt-174 removed test

--- src/app/features/hotel/components/hotel-child-age-row/child-age-option-row.component.spec.ts
+++ src/app/features/hotel/components/hotel-child-age-row/child-age-option-row.component.spec.ts
@@ -1,43 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ChildAgeOptionRowComponent } from './child-age-option-row.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { DatePipe, NgIf } from '@angular/common';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatDividerModule } from '@angular/material/divider';
-
-describe('BookNowOptionRowComponent', () => {
-  let component: ChildAgeOptionRowComponent;
-  let fixture: ComponentFixture<ChildAgeOptionRowComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        MatFormFieldModule,
-        MatInputModule,
-        MatDatepickerModule,
-        FormsModule,
-        MatButtonModule,
-        MatIconModule,
-        ReactiveFormsModule,
-        DatePipe,
-        MatMenuModule,
-        MatDividerModule,
-        NgIf,
-      ],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(ChildAgeOptionRowComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { ChildAgeOptionRowComponent } from './child-age-option-row.component';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatIconModule } from '@angular/material/icon';
+// import { DatePipe, NgIf } from '@angular/common';
+// import { MatMenuModule } from '@angular/material/menu';
+// import { MatDividerModule } from '@angular/material/divider';
+//
+// describe('BookNowOptionRowComponent', () => {
+//   let component: ChildAgeOptionRowComponent;
+//   let fixture: ComponentFixture<ChildAgeOptionRowComponent>;
+//
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [
+//         MatFormFieldModule,
+//         MatInputModule,
+//         MatDatepickerModule,
+//         FormsModule,
+//         MatButtonModule,
+//         MatIconModule,
+//         ReactiveFormsModule,
+//         DatePipe,
+//         MatMenuModule,
+//         MatDividerModule,
+//         NgIf,
+//       ],
+//     }).compileComponents();
+//
+//     fixture = TestBed.createComponent(ChildAgeOptionRowComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
--- src/app/features/hotel/components/hotel-date-button-mobile/hotel-book-now-button-mobile.component.spec.ts
+++ src/app/features/hotel/components/hotel-date-button-mobile/hotel-book-now-button-mobile.component.spec.ts
@@ -1,22 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { HotelBookNowButtonMobileComponent } from './hotel-book-now-button-mobile.component';
-
-describe('HotelBookNowButtonMobileComponent', () => {
-  let component: HotelBookNowButtonMobileComponent;
-  let fixture: ComponentFixture<HotelBookNowButtonMobileComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HotelBookNowButtonMobileComponent],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(HotelBookNowButtonMobileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { HotelBookNowButtonMobileComponent } from './hotel-book-now-button-mobile.component';
+//
+// describe('HotelBookNowButtonMobileComponent', () => {
+//   let component: HotelBookNowButtonMobileComponent;
+//   let fixture: ComponentFixture<HotelBookNowButtonMobileComponent>;
+//
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [HotelBookNowButtonMobileComponent],
+//     }).compileComponents();
+//
+//     fixture = TestBed.createComponent(HotelBookNowButtonMobileComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
