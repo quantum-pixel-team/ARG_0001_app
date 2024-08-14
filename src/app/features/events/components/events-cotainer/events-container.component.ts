2f45ec47126e7ec9482f5d35068eccf2e3bbd88e
Unic-al <forsian.elezi@gmail.com>
2024-08-14 15:06:02 +0200

    [FE] QPT-196 SEO without Google A

--- src/app/features/events/components/events-cotainer/events-container.component.ts
+++ src/app/features/events/components/events-cotainer/events-container.component.ts
@@ -2,17 +2,18 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  OnInit,
   ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
 import { AppEvent } from '../../interfaces/app-event';
 
 import { EventHttpService } from '../../services/event-http.service';
-import { map } from 'rxjs/operators';
+import { map, window } from 'rxjs/operators';
 import { Page } from '../../../../shared/interfaces/page';
 import { PageEvent } from '@angular/material/paginator';
-import { TranslateService } from '@ngx-translate/core';
-import {LanguageService} from "../../../../shared/services/language.service";
+import { LanguageService } from '../../../../shared/services/language.service';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-events-cotainer',
@@ -20,7 +21,7 @@ import {LanguageService} from "../../../../shared/services/language.service";
   styleUrl: './events-container.component.scss',
   encapsulation: ViewEncapsulation.None,
 })
-export class EventsContainerComponent implements AfterViewInit {
+export class EventsContainerComponent implements OnInit,AfterViewInit {
   upcomingEvents: Page<AppEvent> | undefined;
   recentEvents: Page<AppEvent> | undefined;
   pageSizeOptions = [5, 10, 25, 100];
@@ -32,6 +33,8 @@ export class EventsContainerComponent implements AfterViewInit {
   constructor(
     private service: EventHttpService,
     private languageService: LanguageService,
+    private titleService: Title,
+    private metaTagService: Meta,
   ) {
     this.languageCode = this.languageService.currentLang;
   }
@@ -105,5 +108,29 @@ export class EventsContainerComponent implements AfterViewInit {
     this.pageSize = $event.pageSize;
     const query = `pageIndex=${$event.pageIndex}&pageSize=${$event.pageSize}&language=${this.languageCode}`;
     this.fetchUpcomingEvents(query);
+  }
+
+  ngOnInit(): void {
+    const title = 'Aragosta Hotel & Restaurant - Events';
+    const ogTitle = 'Welcome to Aragosta Hotel & Restaurant in Durres  - Events ';
+
+    this.titleService.setTitle(title);
+
+    const keywords = 'event, Durres, meeting, meetings, events, events near me,' +
+      ' events happening, durres, events near me, events in durres, book events, events nearby, aragosta events,' +
+      ' events durres, etc';
+    const description = 'Celebrate in style at Hotel Aragosta, where our sophisticated event spaces and dedicated team ensure every gathering—from weddings to corporate meetings—is a remarkable success. Let us bring your vision to life.';
+    const ogDescription = 'Host unforgettable events at Hotel Aragosta, where our elegant venues and exceptional service create the perfect setting for weddings, conferences, and special occasions. Let us make your event truly memorable.';
+    this.metaTagService.addTags([
+      { name: 'keywords', content: keywords },
+      { name: 'description', content: description },
+      { property: 'og:title', content: ogTitle },
+      { property: 'og:description', content: ogDescription },
+      {
+        property: 'og:image',
+        content: 'https://aragosta-test.imgix.net/A4.PNG?auto=format&w=1080',
+      },
+      { property: 'og:url', content: 'aragosta.al/events' },
+    ]);
   }
 }
--- src/app/features/restaurant/components/restaurant-container/restaurant-container.component.ts
+++ src/app/features/restaurant/components/restaurant-container/restaurant-container.component.ts
@@ -1,16 +1,48 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RestaurantMenuComponent } from '../restaurant-menu/restaurant-menu.component';
 import { RestaurantReservationComponent } from '../restaurant-reservation/restaurant-reservation.component';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-restaurant-container',
   templateUrl: './restaurant-container.component.html',
   styleUrl: './restaurant-container.component.scss',
 })
-export class RestaurantContainerComponent {
+export class RestaurantContainerComponent implements OnInit {
   @ViewChild('menu', { static: false }) menuComponent!: RestaurantMenuComponent;
   @ViewChild('reservation', { static: false })
   reservationComponent!: RestaurantReservationComponent;
+
+  constructor(
+    private titleService: Title,
+    private metaTagService: Meta,
+  ) {}
+
+  ngOnInit(): void {
+    const title = 'Aragosta Hotel & Restaurant - Restaurant';
+    const ogTitle = 'Welcome to Aragosta Hotel & Restaurant in Durres  - Fine Dining Experience';
+    this.titleService.setTitle(title);
+
+    const keywords =
+      'restaurant, Durres, fish, italian cuisine, local ingredients, restaurant reservations, book table, durres, restaurant, restaurants in durres,' +
+      ' book table, restaurants near me,  restaurants nearby, aragosta hotel restaurant, italian restaurants durres, seafood, pizza, pizzeria, fish, etc';
+    const description =
+      'Welcome to Aragosta Hotel & Restaurant in Durres! Since 1995, savor the finest fish and Italian cuisine at our diverse restaurant.' +
+      ' Book a table today to experience fine dining, excellent service, and a cozy atmosphere.';
+    const ogDescription = 'Discover Aragosta Hotel & Restaurant delectable dishes made from the freshest local ingredients. ' +
+      'Reserve a table and enjoy an unforgettable dining experience with us.';
+    this.metaTagService.addTags([
+      { name: 'keywords', content: keywords },
+      { name: 'description', content: description },
+      { property: 'og:title', content: ogTitle },
+      { property: 'og:description', content: ogDescription },
+      {
+        property: 'og:image',
+        content: 'https://aragosta-test.imgix.net/A19.jpg?format=compress&auto=format&w=1080',
+      },
+      { property: 'og:url', content: 'aragosta.al/restaurant' },
+    ]);
+  }
 
   scrollTo(el: HTMLDivElement) {
     el.scrollIntoView({ behavior: 'smooth' });
