d8763c213e205228fe14da4b8c944be0ac425294
Indrit Vaka <63372867+Indrit-Vaka@users.noreply.github.com>
2024-08-19 19:03:57 +0200

    Configured domain with server ip, and BE (#50)
    * [IV] QPT-200 working with performance improvements
    
    * [IV] QPT-200 testing ssl
    
    ---------
    
    Co-authored-by: Indrit Vaka <indrit.vaka@gmail.com>
    Co-authored-by: Luka Buziu <lukabuziu22@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from './environment.model';
 
 export const environment: Environment = {
-  api: 'http://62.171.153.17:8080',
-  envName: 'dev',
+  api: 'https://quantum-pixel.eu:8080',
+  envName: 'production',
 };
