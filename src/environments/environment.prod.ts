68787a9081699ec68e8ec6bf6c12808788c899f6
Indrit Vaka <indrit.vaka@gmail.com>
2024-08-19 21:44:44 +0200

    [IV] QPT-200 testing ssl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from './environment.model';
 
 export const environment: Environment = {
-  api: 'https://quantum-pixel.eu:8080',
+  api: 'https://aragosta.al:8080',
   envName: 'production',
 };
