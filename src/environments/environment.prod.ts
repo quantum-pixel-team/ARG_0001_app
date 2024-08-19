4ff5c42474b8e5c502fb62cb5dc614173d744648
Indrit Vaka <indrit.vaka@gmail.com>
2024-08-19 16:27:31 +0200

    [IV] QPT-200 testing ssl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from './environment.model';
 
 export const environment: Environment = {
-  api: 'http://45.90.121.140:8080',
+  api: 'https://45.90.121.140:8080',
   envName: 'production',
 };
