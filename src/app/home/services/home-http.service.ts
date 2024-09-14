5924b752faa10c40939041619290489e60b25951
Indrit Vaka <indrit.vaka@gmail.com>
2024-09-14 21:56:35 +0200

    [IV] QPT-217 adjusted home restaurant menu and changed second image

--- src/app/home/services/home-http.service.ts
+++ src/app/home/services/home-http.service.ts
@@ -51,26 +51,26 @@ export class HomeHttpService {
       {
         id: 1,
         title: 'Seafood Salad',
-        price: 9,
+        price: 1000,
         imageUrl: 'seafood_salad.jpeg',
       },
       {
         id: 2,
         title: 'Linguini with seafood',
-        price: 8.5,
-        imageUrl: 'rizza_deliziosa.jpeg',
+        price: 950,
+        imageUrl: 'linguini_with_seafood.jpeg',
       },
       {
         id: 3,
         title: 'Pizza Deliziosa',
-        price: 7.5,
-        imageUrl: 'grilled_sea_bass.jpeg',
+        price: 750,
+        imageUrl: 'rizza_deliziosa.jpeg',
       },
       {
         id: 4,
         title: 'Grilled Chicken Fillet',
-        price: 7,
-        imageUrl: 'linguini_with_seafood.jpeg',
+        price: 750,
+        imageUrl: 'grilled_chicken_fillet.jpeg',
       },
     ];
   }
@@ -80,13 +80,13 @@ export class HomeHttpService {
       {
         id: 5,
         title: 'Grilled Sea Bass',
-        price: 12,
-        imageUrl: 'grilled_chicken_fillet.jpeg',
+        price: 1200,
+        imageUrl: 'grilled_sea_bass.jpeg',
       },
       {
         id: 6,
         title: 'Grilled Salmon',
-        price: 12,
+        price: 1200,
         imageUrl: 'grilled_salmon.jpeg',
       },
     ];
