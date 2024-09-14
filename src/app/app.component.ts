cce0ec7ba5c65403d5d23cadb4ed7e2d094276db
Indrit Vaka <indrit.vaka@gmail.com>
2024-09-14 22:08:07 +0200

    [IV] QPT-220 cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,9 @@
-import { Component, HostListener, OnInit, Renderer2 } from "@angular/core";
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { Language } from './shared/interfaces/Language';
 import { LanguageService } from './shared/services/language.service';
-import { DomSanitizer } from "@angular/platform-browser";
-import { MatIconRegistry } from "@angular/material/icon";
-const THUMBUP_ICON =
-  `
-  <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px">
-    <path d="M0 0h24v24H0z" fill="none"/>
-    <path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.` +
-  `44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5` +
-  `1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"/>
-  </svg>
-`;
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -26,16 +18,34 @@ export class AppComponent implements OnInit {
     private renderer: Renderer2,
     private iconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer,
-    ) {}
+  ) {}
 
   ngOnInit(): void {
     this.iconRegistry
-      .addSvgIcon('instagram',this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/instagram.svg'))
-      .addSvgIcon('whatsapp',this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/whatsapp.svg'))
-      .addSvgIcon('facebook',this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/facebook.svg'))
-      .addSvgIcon('tiktok',this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/tiktok.svg'))
-    ;
-
+      .addSvgIcon(
+        'instagram',
+        this.domSanitizer.bypassSecurityTrustResourceUrl(
+          'assets/icons/instagram.svg',
+        ),
+      )
+      .addSvgIcon(
+        'whatsapp',
+        this.domSanitizer.bypassSecurityTrustResourceUrl(
+          'assets/icons/whatsapp.svg',
+        ),
+      )
+      .addSvgIcon(
+        'facebook',
+        this.domSanitizer.bypassSecurityTrustResourceUrl(
+          'assets/icons/facebook.svg',
+        ),
+      )
+      .addSvgIcon(
+        'tiktok',
+        this.domSanitizer.bypassSecurityTrustResourceUrl(
+          'assets/icons/tiktok.svg',
+        ),
+      );
   }
 
   onLanguageChanged(language: Language) {
